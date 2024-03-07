8352cdfee862b1870a9b03cb8051959636ba2dd3
jesijohnkennedy <jesijohnkennedy@gmail.com>
2024-03-07 12:59:51 +0530

    Few lines of coding

---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,42 @@
+package org.bank;
+
+public class BankInfo {
+	private void fullName() {
+		System.out.println(" fullName is Kenendy "); 
+}
+ private void sortCode() {
+	System.out.println("sortCode is 12345");
+
+}
+	private void accountNumber() {
+	
+System.out.println("accountNumber is 20482512582");
+	}
+	private void bankAddress() {
+	System.out.println("bankAddress is Perumbakkam");
+
+	}
+	public static void main(String[] args) {
+		BankInfo d=new BankInfo();
+		d.fullName();
+		d.sortCode();
+		d.accountNumber();
+		d.bankAddress();
+	}
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
